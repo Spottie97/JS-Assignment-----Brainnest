b1db98da56fce6b21b6908b67688a0988ee0be28
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-02-03 11:14:19 +0200

    Rename Files, Assignment 3 (A+B)

---
+++ assignment_3.js
@@ -0,0 +1,16 @@
+/*Large int with if-else*/
+const num1 = 5;
+const num2 = 10;
+if (num1 > num2) {
+  console.log(num1 + " is larger");
+} else {
+  console.log(num2 + " is larger");
+}
+
+/*Check if Even or Odd int*/
+const num = 5;
+if (num % 2 === 0) {
+  console.log(num + " is an even number");
+} else {
+  console.log(num + " is an odd number");
+}
